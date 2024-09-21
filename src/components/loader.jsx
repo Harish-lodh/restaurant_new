86109c3308c1fc94f95f07dfc729d12fbfdfa95f
harish <harishlodh444@gmail.com>
2024-09-21 14:04:37 +0530

    add loading

---
+++ src/components/loader.jsx
@@ -0,0 +1,14 @@
+
+// src/components/Loader.js
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <h2>Loading...</h2>
+      {/* You can add a spinner here if you have one */}
+    </div>
+  );
+};
+
+export default Loader;
